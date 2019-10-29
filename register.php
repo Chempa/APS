ae4d8a8b3963db97b82ae67b9f2bb4951b653e8b
Chempa <30638759+Chempa@users.noreply.github.com>
2019-10-29 17:41:09 +0000

    Update register.php

--- register.php
+++ register.php
@@ -1,25 +1,12 @@
 <?php
 	include("db.php");
-    
-	include("models/users.php");
-    // echo "HELLO";
-    // die();
+	include("models/users.php"); 
 	$a = new User();
 	$ret = $a->create($con,$_POST["phone"],$_POST["password"]);
-    
-	
     if($ret == 1){
         $data = ['success' => 1, 'desc'=>'registration successful'];
-        // echo json_encode($data);
     }else{
-        if($ret == -1){
-            $data = ['success' => -1,'token'=>$a->ustr, 'desc'=>'user is already registered'];
-            // echo json_encode($data);
-        }else{
-            $data = ['success' => 0,'token'=>'null', 'desc'=>'registration failed'];
-            
-        }
+	$data = ['success' => 0,'token'=>$a->ustr, 'desc'=>'registration failed/user already registered'];
     }
-//     header('Content-Type: application/json');
     echo json_encode($data);
 ?>
