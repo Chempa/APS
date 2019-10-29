3b3383340d73706b388cea2f6d53aeac23975f6a
Chempa <30638759+Chempa@users.noreply.github.com>
2019-10-29 17:43:16 +0000

    Update register.php

--- register.php
+++ register.php
@@ -6,7 +6,7 @@
     if($ret == 1){
         $data = ['success' => 1, 'desc'=>'registration successful'];
     }else{
-	$data = ['success' => 0,'token'=>$a->ustr, 'desc'=>'registration failed/user already registered'];
+	$data = ['success' => 0,'token'=>$a->ustr, 'desc'=>'registration failed, user already registered'];
     }
     echo json_encode($data);
 ?>
