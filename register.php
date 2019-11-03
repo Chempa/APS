3a273e48089f88b629ff04dcb3983c91211d04b3
Chempa <30638759+Chempa@users.noreply.github.com>
2019-11-03 13:27:29 +0000

    Update register.php

--- register.php
+++ register.php
@@ -2,11 +2,27 @@
 	include("db.php");
 	include("models/users.php"); 
 	$a = new User();
+	$data = [];
+	$failed = 0;
+	if($_POST["phone"] == "" or $_POST["password"] == ""){
+		$data['desc'] = 'Empty credentials'; $failed = 1;
+	}
+	if(strlen($_POST["phone"]) < 10 or strlen($_POST['password']) < 8){
+		$data['desc'] = "Invalid Phone number or password";$failed = 1;
+	}
+	if (!ctype_digit($_POST["phone"])){
+		$data['desc'] = 'Invalid Phone number'; $failed = 1;
+	}	
+	if($failed == 1){
+		$data['success'] = -1;
+		echo json_encode($data);
+		exit();
+	}
 	$ret = $a->create($con,$_POST["phone"],$_POST["password"]);
-    if($ret == 1){
-        $data = ['success' => 1, 'token'=>$a->ustr,'desc'=>'registration successful'];
-    }else{
+	if($ret == 1){
+	$data = ['success' => 1, 'token'=>$a->ustr,'desc'=>'registration successful'];
+	}else{
 	$data = ['success' => 0,'desc'=>'registration failed, user already registered'];
-    }
-    echo json_encode($data);
+	}
+	echo json_encode($data);
 ?>
