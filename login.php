09100162d2f5419b160c6a6c8419a42f31decf6d
Chempa <30638759+Chempa@users.noreply.github.com>
2019-11-03 13:19:37 +0000

    Update login.php

--- login.php
+++ login.php
@@ -2,10 +2,24 @@
 	include("db.php");
 	include("models/users.php");
 	$a = new User();
+	$data = [];
+	$failed = 0;
+	if($_POST["phone"] == "" or $_POST["password"] == ""){
+		$data['desc'] = 'Empty credentials'; $failed = 1;
+	}
+	if(strlen($_POST["phone"]) < 10 or strlen($_POST['password']) < 8){
+		$data['desc'] = "Invalid Phone number or password";$failed = 1;
+	}
+	if($failed == 1){
+		$data['success'] = -1;
+		echo json_encode($data);
+		exit();
+	}
+	
 	$ret = $a->authenticate($con,$_POST["phone"],$_POST["password"]);
 
     // header('Content-type: application/json');
-    $data = [];
+    
     if($ret == 1){
         // MY METHOD
         // $data = ['success' => 1 ,'token'=>$a->ustr];
